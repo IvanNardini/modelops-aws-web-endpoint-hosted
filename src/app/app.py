9cfa407e2ab9c2b3e7fb51c3c9f0bb88e67c8b54
Ubuntu <ubuntu@ip-10-249-21-248.ec2.internal>
2020-05-30 08:23:08 +0000

    Add app. Starting testing.

---
+++ src/app/app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask
+
+#create an instance
+app = Flask(__name__)
+
+#what URL should trigger our function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+            
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080,debug=True)

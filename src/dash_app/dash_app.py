584d588efefdd7bcbf1e17b9e53c8d3ac9057766
Ubuntu <ubuntu@ip-10-249-21-252.ec2.internal>
2020-06-01 17:03:01 +0000

    Task: Iphone Dash Application. Status: On it's way

--- src/dash_app/dash_app.py
+++ src/dash_app/dash_app.py
@@ -5,18 +5,21 @@ import dash
 import dash_core_components as component
 import dash_html_components as html
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/EugenioPeluso/pen/jOWNmvb.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Dash apps 1st element: layout
-app.layout = html.Div(children=[
-    #Title
-    html.H1(children='Shopping App'),
+app.layout = html.Div(id="iphoneCover", children=[
     
-    html.Div(children=
-             """
-             A Scoring Interactive Web Service for testing
-             """)
+    html.Div(id="form_camp", children=[ 
+        
+        html.P([
+        html.Label('Customer ID: '),
+        component.Input(type='text', id='uid')
+        ])
+    ])
 ])
 
 if __name__ == '__main__':
-    app.run_server(port='8052', debug=True)
+    app.run_server(host="0.0.0.0", debug=True)
